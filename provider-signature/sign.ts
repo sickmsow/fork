7903f63a50e978cd78a5a4b3ce78a7b11591ec92
morgueye4 <gueyemor2009@gmail.com>
2024-12-26 22:21:30 +0000

    fix test + refactor

--- provider-signature/sign.ts
+++ provider-signature/sign.ts
@@ -1,36 +1,16 @@
-  import {
-    mnemonicGenerate,
-    cryptoWaitReady 
-  } from "https://deno.land/x/polkadot@0.2.45/util-crypto/mod.ts";
+import {
+  cryptoWaitReady,
+} from "https://deno.land/x/polkadot@0.2.45/util-crypto/mod.ts";
 
-  import {
-    stringToU8a,
-  } from "https://deno.land/x/polkadot@0.2.45/util/mod.ts";
+import { Keyring } from "https://deno.land/x/polkadot@0.2.45/keyring/mod.ts";
 
-  import { Keyring } from "https://deno.land/x/polkadot@0.2.45/keyring/mod.ts";
-  
-  export async function generateKeypair(): Promise<{ mnemonic: string, pair: any }> {
-    const keyring = new Keyring({ type: "ed25519", ss58Format: 42 });
-    const mnemonic = mnemonicGenerate();
-    // create & add the pair to the keyring some metadata
-    const pair = keyring.addFromUri(mnemonic, { name: "first pair" }, "ed25519");
-    // the pair has been added to our keyring
-    console.log(keyring.pairs.length, "pairs available");
-    // log the name & address (the latter encoded with the ss58Format)
-    console.log(pair.meta.name, "has address", pair.address);
-    return {
-      mnemonic,
-      pair
-    };
-  }
-  
-  export async function signMessage(message: string, mnemonic: string): Promise<Uint8Array> {
-    await cryptoWaitReady();
-    const keyring = new Keyring({ type: "sr25519", ss58Format: 42 });
-    // create a pair based on the provided mnemonic
-    const pair = keyring.addFromUri(mnemonic);
-    // create the message
-    const u8Message = stringToU8a(message);
-    return pair.sign(u8Message);
-  }
-  
+import { stringToU8a } from "https://deno.land/x/polkadot@0.2.45/util/mod.ts";
+
+export async function signMessage(message: string, mnemonic: string) {
+  await cryptoWaitReady();
+  const keyring = new Keyring();
+  const pair = keyring.createFromUri(mnemonic);
+  const u8tifiedMsg = stringToU8a(message);
+  const signature = pair.sign(u8tifiedMsg);
+  return signature;
+}
--- provider-signature/sign_test.ts
+++ provider-signature/sign_test.ts
@@ -1,27 +1,17 @@
-import { assertEquals, assert } from "https://deno.land/std/testing/asserts.ts";
+import { assert } from "@std/assert";
 import { signMessage } from "./sign.ts";
-import { generateKeypair } from "./helpers.ts";
-
+import { Keyring } from "https://deno.land/x/polkadot@0.2.45/keyring/mod.ts";
 import {
-  signatureVerify 
+  signatureVerify,
 } from "https://deno.land/x/polkadot@0.2.45/util-crypto/mod.ts";
 
-Deno.test("generateKeypair generates a valid keypair", async () => {
-  const { mnemonic, pair } = await generateKeypair();
-
-  assert(mnemonic.length > 0, "Mnemonic should not be empty");
-  assert(pair, "Keyring pair should be defined");
-  assertEquals(pair.meta.name, "first pair", "Keyring pair name should be 'first pair'");
-  assert(pair.address.length > 0, "Keyring pair address should not be empty");
-});
-
 Deno.test("signMessage signs a message correctly", async () => {
-  const { mnemonic, pair } = await generateKeypair();
-  const message = "Hello, world!";
-  const signature = await signMessage(message, mnemonic);
-  assert(signature.length > 0, "Signature should not be empty");
-  const isValid = signatureVerify(message, signature, pair.address);
-  console.log(isValid);
-  //TODO: Fix this test
-  //assert(isValid.isValid, "Signature should be valid");
+  const MNEMONIC =
+    "sample split bamboo west visual approve brain fox arch impact relief smile";
+  const keyring = new Keyring();
+  const pair = keyring.createFromUri(MNEMONIC);
+  const message = "this is our message";
+  const signature = await signMessage(message, MNEMONIC);
+  const verif = await signatureVerify(message, signature, pair.address);
+  assert(verif, "Signature is Valid");
 });
